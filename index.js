5cef26c9945fa2e546f84982ef9bb84d88b02e5e
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-12-09 15:45:54 +0200

    have services starting, webhooks generated, and listened for

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-var service = require('./lib/service')
+var ServiceManager = require('./lib/service')
+  , path = require('path')
+  , commander = require('commander')
   , path = require('path')
   ;
 
-service.on('info', function(){
-
-});
-
-service.on('test-started', function(message){
-
-});
-
-service.on('test-ended', function(message){
-
-});
+var service = new ServiceManager();
+var repo;
 
 service.on('stopping', function(message){
+
+});
+
+service.on('error', function(message){
 
 });
 
 service.on('started', function(message){
 
   console.log('Tracey is up and running...');
+
+  if (repo){
+    service.services.queue.push({repo:repo});
+  }
 });
 
+commander
+
+  .version(JSON.parse(require('fs').readFileSync(__dirname + path.sep + 'package.json')).version)
+  .option("-u, --username <val>", "github username")
+  .option("-t, --token <val>", "github token")
+  .option("-r, --repo <val>", "initial run")
+
+  .parse(process.argv);
+
+var username = commander.option('u').username;
+var token = commander.option('t').token;
+repo = commander.option('r').repo;
+
 var config = require('./util/parse-config')(__dirname + path.sep + '.tracey.yml');
+var privateConfig;
+
+if (username && token){
+
+  if (!config.github) config.github = {};
+  if (!config.github.api) config.github.api = {};
+}
+else {
+  try{
+    privateConfig = require('./util/parse-config')(__dirname + path.sep + 'private/config.yml');
+  }catch(e){
+    //do nothing
+  }
+}
+//private overrides (tokens and the like)
+if (privateConfig){
+
+  username = privateConfig.username;
+  token = privateConfig.token;
+}
+
+config.github.username = username;
+config.github.token = token;
+
+console.log('STARTING:::', JSON.stringify(config, null, 2));
 
 service.start(config);
 
