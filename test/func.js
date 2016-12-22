8d027ed1c1647f1ee89a311b00db0c15ee3f6d61
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-12-22 21:40:03 +0200

    no longer fielding proc stop

--- test/func.js
+++ test/func.js
@@ -1,12 +1,5 @@
 describe('functional tests', function () {
 
-  this.timeout(120000);
-
-  it('starts up the service, pops a job on the queue', function (done) {
-
-
-    
-    done();
-  });
+ 
 
 });
--- test/unit.js
+++ test/unit.js
@@ -2,8 +2,44 @@ describe('unit tests', function () {
 
   this.timeout(120000);
 
-  it('tests the queue service, pushes a message in', function (done) {
-    done();
+
+  it('starts up the queue service, pops a job on the queue', function (done) {
+
+
+    var QueueService = require('../lib/queue/service');
+
+    var queueInstance = new QueueService();
+
+    queueInstance.on('message-popped', function(message){
+
+      //console.log('message-popped happened ', message);
+
+      done();
+    });
+
+    queueInstance.__emit('message-popped', {
+        "id": "1482423997794_be289689-dddc-4c2f-89ef-21b9bddd38db",
+        "message": {
+          "repo": "happner/tracey",
+          "event": {
+            "type": "push",
+            "name": "tracey",
+            "owner": "happner",
+            "branch": "refs/heads/master"
+          },
+          "config": {
+            "owner": "happner",
+            "name": "tracey",
+            "test_script": "test-benchmarket",
+            "node_js": [
+              "7",
+              "0.10"
+            ]
+          }
+        }
+      }
+    );
+
   });
 
 });
