f2f8ae4b7e67cac516324dd7dd74af7c17e83095
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-12-23 16:59:31 +0200

    updated queue

--- tracey-test-runner.js
+++ tracey-test-runner.js
@@ -1,9 +1,23 @@
 var sm = require("happner-serial-mocha")
   , path = require("path")
   , fs = require("fs")
+  , pack = require("./package.json")
   ;
 
-var testDir = path.resolve(__dirname, '../');
+var testDir;
+
+if (!pack.tracey){
+
+  testDir = __dirname + path.sep + 'test';
+
+} else {
+
+  if (!pack.tracey.testFolder) throw new Error('tracey configuration needs to specify a test folder');
+
+  if (path.isAbsolute(pack.tracey.testFolder)) testDir = pack.tracey.testFolder;
+
+  else testDir = path.resolve(__dirname, pack.tracey.testFolder);
+}
 
 var files = [];
 
@@ -15,19 +29,18 @@ fs.readdirSync(testDir).forEach(function (filename) {
   if (!file.isDirectory() && filename.indexOf('.js') > -1) files.push(filePath);
 });
 
-var reportDir = path.resolve(__dirname, '../reports');
-
-console.log('running files::::', files);
+var reportDir = testDir + path.sep + 'reports';
 
 sm.runTasks(files, null, reportDir)
 
 //sm.runTasks(files, 'lib/serialReporter.js', true)
 .then(function(results){
+  //dont remove the ::::output results:::: tags, they are used to take out the test results json
   console.log('::::output results::::');
   console.log(JSON.stringify(results, null, 2));
   console.log('::::output results::::');
 })
 
 .catch(function(e){
-  console.log('broke:::', e);
+  console.log('tracey test run broke:::', e);
 });
